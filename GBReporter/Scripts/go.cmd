59e807805aa38a6a48d8b5cca8850607453e6cfb
qmlab <mainchin@msn.com>
2015-03-25 18:21:58 -0700

    update go script

--- GBReporter/Scripts/go.cmd
+++ GBReporter/Scripts/go.cmd
@@ -2,11 +2,11 @@
 
 timeout 0 && start PremierLeague.cmd
 timeout 60 && start ChampionsLeague.cmd
-timeout 120 && start SerieA.cmd
-timeout 180 && start LaLiga.cmd
-timeout 240 && start Bundesliga.cmd
-timeout 300 && start EuropaLeague.cmd
-timeout 360 && start Ligue1.cmd
-timeout 420 && start MLS.cmd
-timeout 480 && start Report.cmd
-timeout 540 && start Result.cmd
+timeout 60 && start SerieA.cmd
+timeout 60 && start LaLiga.cmd
+timeout 60 && start Bundesliga.cmd
+timeout 60 && start EuropaLeague.cmd
+timeout 60 && start Ligue1.cmd
+timeout 60 && start MLS.cmd
+timeout 60 && start Report.cmd
+timeout 60 && start Result.cmd
